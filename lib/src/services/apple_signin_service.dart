38583c957200073aa79038a073b0b235988ed05f
Felipe Lozano <felipelozano@iMac-de-Felipe.local>
2021-08-17 19:08:00 -0700

    AplleSigIn xcode

---
+++ lib/src/services/apple_signin_service.dart
@@ -0,0 +1,17 @@
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+class AppleSignInService {
+  static void signIn() async {
+    final credential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+    );
+
+    print(credential);
+
+    // Now send the credential (especially `credential.authorizationCode`) to your server to create a session
+    // after they have been validated with Apple (see `Integration` section for more information on how to do this)
+  }
+}
