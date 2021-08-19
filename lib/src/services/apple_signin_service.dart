f05e825ff3a7ce07e3bfc1601c6b43ef7adca1af
Felipe Lozano <felipelozano@iMac-de-Felipe.local>
2021-08-18 18:32:28 -0700

    AppleSignIn

--- lib/src/services/apple_signin_service.dart
+++ lib/src/services/apple_signin_service.dart
@@ -2,14 +2,18 @@ import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AppleSignInService {
   static void signIn() async {
-    final credential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-    );
+    try {
+      final credential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
 
-    print(credential);
+      print(credential);
+    } catch (e) {
+      print(e);
+    }
 
     // Now send the credential (especially `credential.authorizationCode`) to your server to create a session
     // after they have been validated with Apple (see `Integration` section for more information on how to do this)
--- lib/src/ui/screens/login_page.dart
+++ lib/src/ui/screens/login_page.dart
@@ -320,11 +320,11 @@ class MyCustomFormState extends State<MyCustomForm> {
                 ), */
               ],
             ),
-            // SignInWithAppleButton(
-            //   text: 'Apple ID',
-            //   onPressed: AppleSignInService.signIn,
+            SignInWithAppleButton(
+              text: 'Apple ID',
+              onPressed: AppleSignInService.signIn,
               
-            // ),
+            ),
             SizedBox(
               height: 16.0,
             ),
