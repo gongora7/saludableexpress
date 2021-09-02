1c1bb28625ae95fdd12ac90d86138f42ad34c370
Cosmonautas <cosmonautasmexico@gmail.com>
2021-09-02 11:56:41 -0500

    Cambios en Intro
    Cambios en Intro + Iconos nuevos

--- lib/src/ui/pages/intro.dart
+++ lib/src/ui/pages/intro.dart
@@ -18,27 +18,46 @@ class _IntroScreenState extends State<IntroScreen> {
     return [
       PageViewModel(
         image: Image.asset(
-          'assets/images/intro-img1.png',
+          'assets/images/iconointro1.png',
         ),
-        title: "Encuentra tus productos",
-        body: "Una gran variedad de productos saludables para tí",
+        title: "Tips para tus viajes",
+        body: "Consulta el blog para conocer de tus próximos destinos",
         footer: Text(''),
         decoration: const PageDecoration(
             titleTextStyle: TextStyle(
                 fontSize: 30.0,
                 fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(241, 90, 36, 1)),
+                color: Color.fromRGBO(229,227,227,1)),
             bodyTextStyle:
                 TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
-            pageColor: Color.fromRGBO(246, 246, 248, 1),
+            pageColor: Color.fromRGBO(255, 88, 56, 1),
             bodyAlignment: Alignment.center,
             imageAlignment: Alignment.center,
             imagePadding: EdgeInsets.only(top: 50.0, left: 40.0)),
       ),
       PageViewModel(
-        image: Image.asset('assets/images/intro-img2.png'),
-        title: "Pagos Seguros",
-        body: "Pagar con cualquier tarjeta, o en transferencia",
+        image: Image.asset('assets/images/iconointro2.png'),
+        title: "Dale vida a tus espacios",
+        body: "Compra fotografías para decorar tu casa u oficina",
+        footer: Text(''),
+        decoration: const PageDecoration(
+            titleTextStyle: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(229,227,227,1)),
+            bodyTextStyle:
+                TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
+            pageColor: Color.fromRGBO(58, 58, 58, 1),
+            bodyAlignment: Alignment.center,
+            imageAlignment: Alignment.center,
+            imagePadding: EdgeInsets.only(top: 50.0, left: 40.0)),
+      ),
+      PageViewModel(
+        image: Image.asset(
+          'assets/images/iconointro3.png',
+        ),
+        title: "Ocasiones Especiales",
+        body: "Consigue regalos para familia y amigos en un solo lugar",
         footer: Text(''),
         decoration: const PageDecoration(
             titleTextStyle: TextStyle(
@@ -54,10 +73,10 @@ class _IntroScreenState extends State<IntroScreen> {
       ),
       PageViewModel(
         image: Image.asset(
-          'assets/images/intro-img3.png',
+          'assets/images/iconointro4.png',
         ),
-        title: "Recibe tu pedido",
-        body: "Disfruta de comprar en línea y mantente saludable",
+        title: "Paga con tarjeta o efectivo",
+        body: "Diferentes métodos de pago para tu comodidad",
         footer: Text(''),
         decoration: const PageDecoration(
             titleTextStyle: TextStyle(
@@ -66,7 +85,26 @@ class _IntroScreenState extends State<IntroScreen> {
                 color: Color.fromRGBO(241, 90, 36, 1)),
             bodyTextStyle:
                 TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
-            pageColor: Color.fromRGBO(246, 246, 248, 1),
+            pageColor: Color.fromRGBO(209, 57, 31, 1),
+            bodyAlignment: Alignment.center,
+            imageAlignment: Alignment.center,
+            imagePadding: EdgeInsets.only(top: 50.0, left: 40.0)),
+      ),
+      PageViewModel(
+        image: Image.asset(
+          'assets/images/iconointro5.png',
+        ),
+        title: "Podcast de Viajes",
+        body: "Entrevista con diferentes viajeros sobre sus experiencias",
+        footer: Text(''),
+        decoration: const PageDecoration(
+            titleTextStyle: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(241, 90, 36, 1)),
+            bodyTextStyle:
+                TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
+            pageColor: Color.fromRGBO(40, 35, 34, 1),
             bodyAlignment: Alignment.center,
             imageAlignment: Alignment.center,
             imagePadding: EdgeInsets.only(top: 50.0, left: 40.0)),
